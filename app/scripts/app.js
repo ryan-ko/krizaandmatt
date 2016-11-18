45df185a673a5f9df6a653311965b2127ebdf3f0
Ryan Ko <rko@ryanko.me>
2016-11-18 21:23:15 +0800

    Adjusting color

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -31,17 +31,13 @@ RKO.APP = (function(window) {
 	};
 
 	app.isCharacterKeyPress = function(evt) {
-			if (typeof evt.which == "undefined") {
-					// This is IE, which only fires keypress events for printable keys
-					return true;
-			} else if (typeof evt.which == "number" && evt.which > 0) {
-					// In other browsers except old versions of WebKit, evt.which is
-					// only greater than zero if the keypress is a printable key.
-					// We need to filter out backspace and ctrl/alt/meta key combinations
-					return !evt.ctrlKey && !evt.metaKey && !evt.altKey && evt.which != 8;
-			}
-			return false;
-	}
+		if (typeof evt.which == "undefined") {
+			return true;
+		} else if (typeof evt.which == "number" && evt.which > 0) {
+			return !evt.ctrlKey && !evt.metaKey && !evt.altKey && evt.which != 8;
+		}
+		return false;
+	};
 
 	app.init = function() {
 		$('#secretword-input').keydown(function(e) {
